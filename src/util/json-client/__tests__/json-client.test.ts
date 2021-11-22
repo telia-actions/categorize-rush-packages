90afff1ef5a5311f67dd5cc2108905026dd380a7
Aidas Vyšniauskas <aidas.vysniauskas@teliacompany.com>
2021-11-22 10:16:44 +0200

    Replace rush.json input with rush projects (#3)
    Replace rusj.json input with rush projects

--- src/util/json-client/__tests__/json-client.test.ts
+++ src/util/json-client/__tests__/json-client.test.ts
@@ -1,18 +1,11 @@
-import { readJson, readJsonWithComments } from '../json-client';
 import jsonMock from '../__mocks__/mock';
+import { readJson } from '../json-client';
 
 describe('json client utilities', () => {
   describe('readJson method', () => {
     const path = 'src/util/json-client/__mocks__/mock.json';
     it('should parse json file content', () => {
       const result = readJson(path);
-      expect(result).toEqual(jsonMock);
-    });
-  });
-  describe('readJsonWithComments method', () => {
-    const path = 'src/util/json-client/__mocks__/mock-comments.json';
-    it('should parse json file content', () => {
-      const result = readJsonWithComments(path);
       expect(result).toEqual(jsonMock);
     });
   });
